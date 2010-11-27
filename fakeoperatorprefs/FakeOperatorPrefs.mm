2fd732a4b86cfb97044653d5174fadf84ce45f42
DarkMalloc <darkmalloc@gmail.com>
2010-11-27 18:56:14 +0000

    Added Preferences

--- fakeoperatorprefs/FakeOperatorPrefs.mm
+++ fakeoperatorprefs/FakeOperatorPrefs.mm
@@ -1,10 +1,42 @@
 #import <Preferences/Preferences.h>
 
 @interface FakeOperatorPrefsListController: PSListController {
+	UITextField * opField;
 }
 @end
 
 @implementation FakeOperatorPrefsListController
+
+- (void)default:(id)item {
+	
+}
+
+- (void)changeOperatorName:(id)item {
+	UIAlertView *dialog = [[UIAlertView alloc] init];
+	[dialog setDelegate:self];
+	[dialog setTitle:@"Enter Operator Name"];
+	[dialog setMessage:@"\n"];
+	[dialog addButtonWithTitle:@"Cancel"];
+	[dialog addButtonWithTitle:@"Change"];
+	opField = [[UITextField alloc] initWithFrame:CGRectMake(20.0, 45.0, 245.0, 25.0)];
+	[opField setBackgroundColor:[UIColor whiteColor]];
+	[opField setPlaceholder:@"Operator name"];
+	[dialog addSubview:opField];
+	[dialog show];
+	[dialog becomeFirstResponder];
+
+}
+
+- (void) alertView:(UIAlertView *)alert clickedButtonAtIndex:(NSInteger)buttonIndex{    
+        NSLog(@"%d", (int) buttonIndex);
+        if (buttonIndex == 1) { // Change pushed
+			NSLog(@"Op name: %@", [opField text]);
+			id controller = [NSClassFromString(@"SBTelephonyManager") sharedTelephonyManager];
+			[controller setOperatorName:[opField text]];
+        } else {
+		}
+}
+
 - (id)specifiers {
 	if(_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"FakeOperatorPrefs" target:self] retain];
